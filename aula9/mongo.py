2016190c55cfa8b32b49a448162d7a3772872939
Gabriel Pimenta <gabriel.bonfim@4linux.com.br>
2017-11-30 21:57:37 -0200

    MEU COMMIT

--- aula9/mongo.py
+++ aula9/mongo.py
@@ -4,34 +4,34 @@ client = MongoClient('127.0.0.1')
 db = client['escola']
 
 # INSERIR DOCUMENTO NO MONGO
-# db.aluno.insert(
-#     {
-#         "nome": "Joao da Silva",
-#         "email": "joao@joao.com.br",
-#     }
-# )
+db.aluno.insert(
+    {
+        "nome": "Joao da Silva",
+        "email": "joao@joao.com.br",
+    }
+)
 
 # BUSCAR ALUNO ESPECIFICO
-# aluno = db.aluno.find({"email": "gabriel@gabriel.com.br"})
-# for a in aluno:
-#     print(a)
+aluno = db.aluno.find({"email": "gabriel@gabriel.com.br"})
+for a in aluno:
+    print(a)
 
 # COMPARAR IDS NO MONGO
-# from bson.objectid import ObjectId
-# alunos = db.aluno.find({})
-# for a in alunos:
-#     if ObjectId('5a1ecf542a2cd623581dd5e8') == a['_id']:
-#         print("AGORA SIM")
+from bson.objectid import ObjectId
+alunos = db.aluno.find({})
+for a in alunos:
+    if ObjectId('5a1ecf542a2cd623581dd5e8') == a['_id']:
+        print("AGORA SIM")
 
 # ADICIONAR CAMPO EM DOCUMENTO QUE JA EXISTE
-# db.aluno.update(
-#     {"email": "joao@joao.com.br"},
-#     {
-#         "$set": {
-#             "etnia": "indio"
-#         }
-#     }
-# )
+db.aluno.update(
+    {"email": "joao@joao.com.br"},
+    {
+        "$set": {
+            "etnia": "indio"
+        }
+    }
+)
 
 # ALTERAR CAMPO NO DOCUMENTO
 # db.aluno.update(
@@ -51,4 +51,9 @@ db.aluno.update(
             "cursos.0.nome": "JAVA AVANCADO"
         }
     }
+)
+
+# REMOVER DOCUMENTO
+db.aluno.remove(
+	{"email": "joao@joao.com.br"}
 )
